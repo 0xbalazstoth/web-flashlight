0b6fdbe7d4cf82040d0167697bd2e2fe44236c97
0xbalazs <grandmastar27@gmail.com>
2020-12-20 22:02:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function turnOff() {
+    let light = document.getElementById("light");
+    light.style.backgroundColor = "transparent";
+    light.style.opacity = "";
+}
+
+function turnOn() {
+    let light = document.getElementById("light");
+    light.style.backgroundColor = "white";
+}
